446ecce36c531d20c9eb2edd3df5359731d7108b
lihim <martin.lihi@gmail.com>
2018-02-25 11:56:39 +0200

    forst commit

---
+++ src/test/java/testCases.java
@@ -0,0 +1,16 @@
+import org.testng.annotations.Test;
+
+public class testCases {
+
+
+
+    @Test
+    public void test1(){
+        System.out.print("test 1 is running");
+    }
+
+    @Test
+    public void test2(){
+        System.out.print("test 2 is running");
+    }
+}
